fe500531123fdca7b9f5b1fcecac07fbb4b69892
ryanapw <144086505+ryanapw@users.noreply.github.com>
2026-06-09 08:30:46 +0700

    Update login.php

--- login.php
+++ login.php
@@ -2,13 +2,11 @@
 header("Content-Type: application/json");
 include "koneksi.php";
 
-// Validasi request agar aman jika diakses langsung dari browser (GET)
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     
     $username = isset($_POST['username']) ? mysqli_real_escape_string($conn, $_POST['username']) : '';
     $password = isset($_POST['password']) ? mysqli_real_escape_string($conn, $_POST['password']) : '';
 
-    // Mengubah dari 'admin' menjadi 'tb_admin'
     $sql = "SELECT * FROM tb_admin WHERE username='$username' AND password='$password'";
     $query = mysqli_query($conn, $sql);
 
